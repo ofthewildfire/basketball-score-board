41646b5c2fa2e5ef687bb2949fd694d456b57dcf
ofthewildfire <lissajace@gmail.com>
2022-07-09 18:59:45 +0200

    added: new game reset button, that resets all values and not just visual

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScore = 0
+let homeScoreLog = document.getElementById("home-score")
+
+let guestScore = 0
+let guestScoreLog = document.getElementById("guest-score")
+
+// console.log(homeScore)
+// interesting, I have to ask or Google why when I moved that let variable down to beneath the home functions it suddenly said it was not working. should it not have worked 'js reads top to bottom' so it was still BEFORE and declared first? /thinking-face. 
+
+// Home functions 
+
+function add1home() {
+  let homeScoreNew = homeScore += 1;
+  homeScoreLog.textContent = homeScoreNew;
+}
+
+function add2home() {
+  let homeScoreNew = homeScore += 2;
+  homeScoreLog.textContent = homeScoreNew;
+}
+
+function add3home() {
+  let homeScoreNew = homeScore += 3;
+  homeScoreLog.textContent = homeScoreNew;
+}
+
+// Guest functions 
+
+function add1guest() {
+  let guestScoreNew = guestScore += 1;
+  guestScoreLog.textContent = guestScoreNew;
+}
+
+function add2guest() {
+  let guestScoreNew = guestScore += 2;
+  guestScoreLog.textContent = guestScoreNew;
+}
+
+function add3guest() {
+  let guestScoreNew = guestScore += 3;
+  guestScoreLog.textContent = guestScoreNew;
+}
